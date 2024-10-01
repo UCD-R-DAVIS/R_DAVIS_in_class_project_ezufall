a621af8706ace0ba496409331af10a3613f979e5
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2024-10-01 13:30:28 -0700

    I wrote my name

---
+++ Week2_IntrotoR.R
@@ -0,0 +1,64 @@
+3 + 4
+
+4+8*3^2
+
+#this is a comment
+
+2/1000000
+4e3
+
+exp(1)
+
+exp(3)
+
+log(100)
+log(100, base = 10)
+y <- 1
+log(y)
+
+log(10, 100)
+log(base = 10, x = 100)
+
+x
+y
+log(x)
+x
+
+rm(x)
+rm(y)
+
+log(-40)
+
+x <- 1
+x <- 3
+
+x = 2
+
+x == 5
+x != 5
+
+x > 4
+x < 3
+
+x >=2
+
+x <= -1
+
+
+x <- x + 1
+x
+y <- x + 2
+
+x + y
+
+numSamples <- 50
+
+num_samples <- 50
+
+numsamples <- 40
+
+rm(numsamples)
+rm(numSamples)
+
+num_samples
+num_samples
