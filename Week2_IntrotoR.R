b35d45a039256e734a65283acf5f08775ef06279
E.J. Zufall <90878994+ezufall@users.noreply.github.com>
2024-10-03 13:55:45 -0700

    week 2 in class work

--- Week2_IntrotoR.R
+++ Week2_IntrotoR.R
@@ -1,15 +1,33 @@
+
+
+
+
+
+
+
+
+
 #Introduction to R: arithmetic
+
 3 + 4
+
+
+
+
+
+
 #incomplete command
-2 *
+2 * 5
+
+
 
 #order of operations
-4 + 8 * 3 ^ 2
+4 + (8 * 3) ^ 2
 
 #this is a comment
 
 #scientific notation
-2/1000000
+2 / 100000
 4e3
 
 #mathematical functions
@@ -19,8 +37,19 @@ exp(3)
 
 log(4)
 
+sqrt(4)
+
+
 
 #r help files
+?log
+log(2, 4)
+log(4, 2)
+log(base = 4, x = 2)
+
+x <- 1
+x
+rm(x)
 #note:
 #?? searches the text of all R help files, e.g. ??base will find log.
 #other places to check out:
@@ -30,98 +59,58 @@ log(4)
 #Cookbook for R http://www.cookbook-r.com/
 
 #arguments to functions
-?log
-log(4, base = 2)
 
-y <- 1
-
-log(y)
-
-log(2, 4)
-
-log(base = 2, x = 4)
-
-x
-
-y
-
-log(x)
-x
-
-rm(x)
-
-rm(y)
 
 
 #nested functions
 sqrt(exp(4))
+log(exp(100))
 
 
 #six comparison functions
-x == 5
+mynumber <- 6
+#two equal signs!!
+mynumber == 5
 
-x != 5
+mynumber != 5
 
-x > 4
+mynumber > 4
 
-x < 3
+mynumber < 3
 
-x >=2
+mynumber >= 2
 
-x <= -1
-
-
-
+mynumber <= -1
 
 #objects and assignment
+mynumber <- 7
 
-x <- x + 1
+othernumber <- -3
 
-x
-
-y <- x + 2
-
-x <- 1
-
-x <- 3
-
-x = 2
-
-x + y
+mynumber * othernumber
 
 #object name conventions
 numSamples <- 50
-
-num_samples <- 50
-
-numsamples <- 40
-
-rm(numsamples)
-rm(numSamples)
-
+num_samples <- 40
+rm(num_samples)
+ls()
 #tab completion
-num_samples
-num_samples
+rm(list = ls())
 
 #errors and warnings
-log("a_word")
 log_of_word <- log("a_word")
 log_of_word
 
 log_of_negative <- log(-2)
 log_of_negative
-
 #challenge
 elephant1_kg <- 3492
 elephant2_lb <- 7757
 
 elephant1_lb <- elephant1_kg * 2.2
 
-elephant1_lb
-elephant2_lb
-
 elephant2_lb > elephant1_lb
 
 myelephants <- c(elephant1_lb, elephant2_lb)
-which(myelephants == max(myelephants))
 
+which(myelephants == max(myelephants))
